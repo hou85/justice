e925242c3edf6017470225cc6c30a89e4dffb1fa
EC2 Default User <ec2-user@ip-172-31-45-4.eu-west-3.compute.internal>
2024-06-30 16:12:04 +0000

    add inventory

--- infra/terraform/inventory.py
+++ infra/terraform/inventory.py
@@ -20,49 +20,64 @@ def main():
     mongodb_ip      = terraform_output.get('mongodb_ip', {}).get('value', [])
     logging_ip      = terraform_output.get('logging_ip', {}).get('value', [])
     monitoring_ip   = terraform_output.get('monitoring_ip', {}).get('value', [])
-    if master_ip:
-        inventory_master = {
-            'all': {
-                'vars': {
-                    'ansible_user': 'ec2-user',
-                    'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
-                }
-            },
+    
+    inventory= {
+        'all': {
+            'vars': {
+                'ansible_user': 'ec2-user',
+                'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
+            }
+        },
             'master': {
                 'hosts': [master_ip],
                     'vars': {
                         'ansible_user': 'ec2-user',
                         'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
                     }
+            },
+            'worker': {
+                'hosts': [worker_ips],
+                    'vars': {
+                        'ansible_user': 'ec2-user',
+                        'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
+                    }
+            },
+            'external': {
+                'hosts': [external_ip],
+                    'vars': {
+                        'ansible_user': 'ec2-user',
+                        'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
+                    }
+            },
+            'internal': {
+                'hosts': [internal_ip],
+                    'vars': {
+                        'ansible_user': 'ec2-user',
+                        'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
+                    }
+            },
+            'mongodb': {
+                'hosts': [mongodb_ip],
+                    'vars': {
+                        'ansible_user': 'ec2-user',
+                        'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
+                    }
+            },
+            'logging': {
+                'hosts': [logging_ip],
+                    'vars': {
+                        'ansible_user': 'ec2-user',
+                        'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
+                    }
+            },
+            'monotoring': {
+                'hosts': [monitoring_ip],
+                    'vars': {
+                        'ansible_user': 'ec2-user',
+                        'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
+                    }
             }
         }
-    else:
-        print("No instance IP found", file=sys.stderr)
-        inventory_master = {}    
-      
-    print(json.dumps(inventory_master, indent=2))
-
-    # if worker_ips:
-    #     inventory_worker = {
-    #         'all': {
-    #             'vars': {
-    #                 'ansible_user': 'ec2-user',
-    #                 'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
-    #             }
-    #         },
-    #         'worker': {
-    #             'hosts': [worker_ips],
-    #                 'vars': {
-    #                     'ansible_user': 'ec2-user',
-    #                     'ansible_ssh_private_key_file': '~/.ssh/terraform.pem'
-    #                 }
-    #         }
-    #     }
-    # else:
-    #     print("No instance IP found", file=sys.stderr)
-    #     inventory_worker = {}
-        
-    # print(json.dumps(inventory_worker, indent=2))
-
+    
 if __name__ == '__main__':
     main()
